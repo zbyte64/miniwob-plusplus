89814f41a8e436c275cb27327e66537c243ad6fc
Jason Kraus <zbyte64@gmail.com>
2019-12-01 11:05:31 -0800

    fix wheel package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     license="Apache 2.0",
     packages=find_packages(where="python", exclude=["test"]),
     package_dir={"": "python"},
+    include_package_data=True,
     version="0.0.1",
     install_requires=["Pillow>=4.0", "selenium>=3.4.3"],
 )
